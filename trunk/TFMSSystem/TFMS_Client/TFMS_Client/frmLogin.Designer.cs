212f0abf7cb7d3f1ab8a66420c85916177c2e0d3
lazer382 <lazer382@85840932-c7fe-11de-b6dc-25c59a28ecba>
2009-11-02 22:47:47 +0000

    added client / server

---
+++ trunk/TFMSSystem/TFMS_Client/TFMS_Client/frmLogin.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace TFMS_Client
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.groupBoxInput = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnCancelConnect = new System.Windows.Forms.Button();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBoxInput.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(102, 41);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 150);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "username:";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(72, 15);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(105, 20);
+            this.txtUser.TabIndex = 3;
+            // 
+            // groupBoxInput
+            // 
+            this.groupBoxInput.Controls.Add(this.txtServer);
+            this.groupBoxInput.Controls.Add(this.label1);
+            this.groupBoxInput.Controls.Add(this.txtUser);
+            this.groupBoxInput.Controls.Add(this.btnConnect);
+            this.groupBoxInput.Location = new System.Drawing.Point(13, 169);
+            this.groupBoxInput.Name = "groupBoxInput";
+            this.groupBoxInput.Size = new System.Drawing.Size(189, 74);
+            this.groupBoxInput.TabIndex = 4;
+            this.groupBoxInput.TabStop = false;
+            this.groupBoxInput.Text = "Login";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblStatus);
+            this.groupBox1.Controls.Add(this.btnCancelConnect);
+            this.groupBox1.Location = new System.Drawing.Point(13, 301);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(189, 74);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(14, 18);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(163, 13);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // btnCancelConnect
+            // 
+            this.btnCancelConnect.Location = new System.Drawing.Point(102, 41);
+            this.btnCancelConnect.Name = "btnCancelConnect";
+            this.btnCancelConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelConnect.TabIndex = 0;
+            this.btnCancelConnect.Text = "Cancel";
+            this.btnCancelConnect.UseVisualStyleBackColor = true;
+            this.btnCancelConnect.Click += new System.EventHandler(this.btnCancelConnect_Click);
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(6, 43);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(90, 20);
+            this.txtServer.TabIndex = 4;
+            this.txtServer.Text = "127.0.0.1";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 253);
+            this.Controls.Add(this.groupBoxInput);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmLogin";
+            this.Text = "TFMS - Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBoxInput.ResumeLayout(false);
+            this.groupBoxInput.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.GroupBox groupBoxInput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnCancelConnect;
+        private System.Windows.Forms.TextBox txtServer;
+    }
+}
+
