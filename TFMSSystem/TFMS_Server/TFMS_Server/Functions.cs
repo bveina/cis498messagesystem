b6b49059803db47f987a7ee94bfff5a20dbe6855
lazer382 <lazer382@85840932-c7fe-11de-b6dc-25c59a28ecba>
2009-11-02 22:47:47 +0000

    added client / server

---
+++ TFMSSystem/TFMS_Server/TFMS_Server/Functions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TFMS_Server
+{
+    public delegate void timerTick(object sender, EventArgs e);
+
+    class Functions
+    {
+        public static void startTimer(ref Timer timer, int rate)
+        {
+            timer = new Timer();
+            timer.Interval = rate;
+            timer.Start();
+        }
+
+        public static void stopTimer(ref Timer timer)
+        {
+            if (timer != null)
+            {
+                timer.Stop();
+                timer.Dispose();
+            }
+        }
+    }
+}
