ea6fb1106039d6f1c1ac6349c8e3fb8c5c09200b
Matthew.Lemanski <Matthew.Lemanski@85840932-c7fe-11de-b6dc-25c59a28ecba>
2010-04-30 10:20:49 +0000

    - added comments to the TFMS_Server_Main class

--- TFMSServer/Console Server/TFMS_Server_Main.cs
+++ TFMSServer/Console Server/TFMS_Server_Main.cs
@@ -8,20 +8,32 @@ using System.Configuration;
 
 namespace Console_Server
 {
+    /// <summary>
+    /// The TFMS_Server_Main class provides one of the Main methods for running the TFMS program
+    /// TFMS_Server_Main starts a TFMS_Server object, which is intended to remain running for the duration of the product
+    /// The server is supposed to be "always on"
+    /// </summary>
     class TFMS_Server_Main
     {
         static void Main(string[] args)
         {
+            // create the server object
             TFMS_Server myServer = new TFMS_Server();
 
+            // initialize the server delegates
             myServer.listRequested += new TFMS_MessageRecieved(handleListRequest);
             myServer.logonRequested += new TFMS_MessageRecieved(handleLoginRequest);
             myServer.logoffRequested += new TFMS_MessageRecieved(handleLogoffRequest);
             myServer.relayRequested += new TFMS_MessageRecieved(handleRelayRequest);
+
+            // start the server
             myServer.startServer();
             
             string input = Console.ReadLine();
             
+            // while the server is running, only listen for 2 different console commands
+            // "x" exits the server program
+            // "c" displays the current client list
             while (input != "x")
             {
                 switch (input)
@@ -40,49 +52,87 @@ namespace Console_Server
                 input = Console.ReadLine();
             }
         }
-        static void handleLoginRequest(TFMS_Data d)
+
+        #region TFMS_Server_Main commands
+
+        /// <summary>
+        /// If the current TFMS_Data objects contains the Login command, echo the status (and perform the command elsewhere)
+        /// </summary>
+        /// <param name="data">TFMS_Data object containing the Login command</param>
+        static void handleLoginRequest(TFMS_Data data)
         {
-            Console.WriteLine("{0} is logging in", d.strName);
-        }
-        static void handleLogoffRequest(TFMS_Data d)
-        {
-            Console.WriteLine("{0} is logging off", d.strName);
-        }
-        static void handleListRequest(TFMS_Data d)
-        {
-            Console.WriteLine("{0} has requested a list of clients", d.strName);
+            Console.WriteLine("{0} is logging in", data.strName);
         }
 
-        static void handleRelayRequest(TFMS_Data d)
+        /// <summary>
+        /// If the current TFMS_Data objects contains the Logoff command, echo the status (and perform the command elsewhere)
+        /// </summary>
+        /// <param name="data">TFMS_Data object containing the Logoff command</param>
+        static void handleLogoffRequest(TFMS_Data data)
         {
-            Console.WriteLine("{0} has sent a message", d.strName);
+            Console.WriteLine("{0} is logging off", data.strName);
+        }
+
+        /// <summary>
+        /// If the current TFMS_Data objects contains the List command, echo the status (and perform the command elsewhere)
+        /// </summary>
+        /// <param name="data">TFMS_Data object containing the List command</param>
+        static void handleListRequest(TFMS_Data data)
+        {
+            Console.WriteLine("{0} has requested a list of clients", data.strName);
+        }
+
+        /// <summary>
+        /// If the current TFMS_Data objects contains the Relay command, echo the status (and perform the command elsewhere)
+        /// When a message is sent to be broadcast to other uses, attempt to log it in the SQL database
+        /// </summary>
+        /// <param name="data">TFMS_Data object containing the Relay command</param>
+        static void handleRelayRequest(TFMS_Data data)
+        {
+            Console.WriteLine("{0} has sent a message", data.strName);
             
             // log the message in the SQL database
             //logMessage(d);
         }
 
-        static void logMessage(TFMS_Data d)
-        {
-            Console.WriteLine("Logging {0}'s message to database", d.strName);
+        #endregion
 
-            //Log the message to the sql database
+        #region TFMS_Server_Main database logging
+
+        /// <summary>
+        /// Attempt to log the current message in the SQL database
+        /// </summary>
+        /// <param name="data">TFMS_Data object containing the current message</param>
+        static void logMessage(TFMS_Data data)
+        {
+            Console.WriteLine("Logging {0}'s message to database", data.strName);
+
+            // Attmpt to log the message to the SQL database
             try
             {
+                // open a new SQL connection
                 SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnect"].ToString());
                 conn.Open();
 
+                // add the message to the database
                 SqlCommand cmd = new SqlCommand("INSERT INTO TFMessage (sender, date, message) VALUES (@sent_by, @date, @msg)", conn);
-                cmd.Parameters.AddWithValue("@sent_by", d.strName);
-                cmd.Parameters.AddWithValue("@date", d.timeStamp);
-                cmd.Parameters.AddWithValue("@msg", d.strMessage);
+                cmd.Parameters.AddWithValue("@sent_by", data.strName);
+                cmd.Parameters.AddWithValue("@date", data.timeStamp);
+                cmd.Parameters.AddWithValue("@msg", data.strMessage);
                 cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Logged {0}'s message.", data.strName);
+
+                // close the connection
                 conn.Close();
-                Console.WriteLine("Logged {0}'s message.", d.strName);
             }
             catch (Exception e)
             {
-                Console.WriteLine("Logging of {0}'s message failed. {1}", d.strName, e.Message);
+                // print to the console if the logging failed
+                Console.WriteLine("Logging of {0}'s message failed. {1}", data.strName, e.Message);
             }
         }
+
+        #endregion
     }
 }
