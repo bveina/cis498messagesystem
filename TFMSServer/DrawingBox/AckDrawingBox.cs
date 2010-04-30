e036b627ff0ebe6007b3a613055d6996e53c667f
Matthew.Lemanski <Matthew.Lemanski@85840932-c7fe-11de-b6dc-25c59a28ecba>
2010-04-30 10:04:35 +0000

    - renamed class "Drawingbox2" to "AckDrawingBox" - reorganized and commented the AckDrawingBox class

--- TFMSServer/DrawingBox/AckDrawingBox.cs
+++ TFMSServer/DrawingBox/AckDrawingBox.cs
@@ -9,14 +9,55 @@ using System.Drawing.Drawing2D;
 
 namespace DrawingBox
 {
-    public partial class DrawingBox2 : UserControl
+    /// <summary>
+    /// AckDrawingBox is the control used to display received messages
+    /// </summary>
+    public partial class AckDrawingBox : UserControl
     {
+        #region AckDrawingBox class variables
+
+        /// <summary>
+        /// The list of PathData that represents the received message
+        /// </summary>
         private List<PathData> myPaths;
+
+        /// <summary>
+        /// A flag to indicate whether or not it is safe to draw
+        /// </summary>
         private bool shouldPaint;
+
+        /// <summary>
+        /// The last known drawn location
+        /// </summary>
         private Point lastLocation;
+
+        /// <summary>
+        /// The current path that is being drawn
+        /// </summary>
         private PathData currentPath;
+
+        /// <summary>
+        /// The color of the current path being drawn
+        /// </summary>
         private Color myColor;
+
+        /// <summary>
+        /// The width of the current path being drawn
+        /// </summary>
         private int myWidth;
+
+        #endregion
+
+        #region AckDrawingBox constructors
+
+        public AckDrawingBox()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region AckDrawingBox "get" and "set" overridden methods
 
         public Color lineColor
         {
@@ -29,10 +70,29 @@ namespace DrawingBox
             set { myWidth = value; }
         }
 
-        public DrawingBox2()
+        #endregion
+
+        #region AckDrawingBox helper methods
+
+        public void Clear()
         {
-            InitializeComponent();
+            myPaths.Clear();
+            currentPath = new PathData();
+            this.Invalidate();
         }
+
+        public Bitmap getImage()
+        {
+            Bitmap temp = new Bitmap(this.Width, this.Height);
+            Graphics g = Graphics.FromImage(temp);
+            foreach (PathData pd in myPaths)
+                g.DrawPath(new Pen(pd.pathColor, pd.pathWidth), pd.path);
+            return temp;
+        }
+
+        #endregion
+
+        #region AckDrawingBox event actions
 
         private void DrawingBox_MouseDown(object sender, MouseEventArgs e)
         {
@@ -89,12 +149,6 @@ namespace DrawingBox
             }
         }
 
-        public void Clear()
-        {
-            myPaths.Clear();
-            currentPath = new PathData();
-            this.Invalidate();
-        }
 
         private void DrawingBox_Load(object sender, EventArgs e)
         {
@@ -102,20 +156,13 @@ namespace DrawingBox
             myPaths = new List<PathData>();
         }
 
-        public Bitmap getImage()
-        {
-            Bitmap temp=new Bitmap(this.Width,this.Height);
-            Graphics g = Graphics.FromImage(temp);
-            foreach (PathData pd in myPaths)
-                g.DrawPath(new Pen(pd.pathColor, pd.pathWidth), pd.path);
-            return temp;
-        }
-
         private void DrawingBox_Resize(object sender, EventArgs e)
         {
-            DrawingBox2 mySender = (DrawingBox2)sender;
+            AckDrawingBox mySender = (AckDrawingBox)sender;
             mySender.Invalidate();
         }
+
+        #endregion
     }
     
 }
