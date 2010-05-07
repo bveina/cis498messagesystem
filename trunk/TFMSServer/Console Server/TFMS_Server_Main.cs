e5ab134c602805ffcc21487bc5d48dc169260788
Matthew.Lemanski <Matthew.Lemanski@85840932-c7fe-11de-b6dc-25c59a28ecba>
2010-05-07 05:14:11 +0000

    VectorBox freaked out when I tried rearranging "get" and "set" stuff inside, so reverted those. in TFMS_Server_Main, made helper methods private and Main method public

--- trunk/TFMSServer/Console Server/TFMS_Server_Main.cs
+++ trunk/TFMSServer/Console Server/TFMS_Server_Main.cs
@@ -13,9 +13,9 @@ namespace Console_Server
     /// TFMS_Server_Main starts a TFMS_Server object, which is intended to remain running for the duration of the product
     /// The server is supposed to be "always on"
     /// </summary>
-    class TFMS_Server_Main
+    public class TFMS_Server_Main
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             // create the server object
             TFMS_Server myServer = new TFMS_Server();
@@ -53,13 +53,13 @@ namespace Console_Server
             }
         }
 
-        #region TFMS_Server_Main commands
+        #region TFMS_Server_Main private helper methods
 
         /// <summary>
         /// If the current TFMS_Data objects contains the Login command, echo the status (and perform the command elsewhere)
         /// </summary>
         /// <param name="data">TFMS_Data object containing the Login command</param>
-        static void handleLoginRequest(TFMS_Data data)
+        private static void handleLoginRequest(TFMS_Data data)
         {
             Console.WriteLine("{0} is logging in", data.strName);
         }
@@ -68,7 +68,7 @@ namespace Console_Server
         /// If the current TFMS_Data objects contains the Logoff command, echo the status (and perform the command elsewhere)
         /// </summary>
         /// <param name="data">TFMS_Data object containing the Logoff command</param>
-        static void handleLogoffRequest(TFMS_Data data)
+        private static void handleLogoffRequest(TFMS_Data data)
         {
             Console.WriteLine("{0} is logging off", data.strName);
         }
@@ -77,7 +77,7 @@ namespace Console_Server
         /// If the current TFMS_Data objects contains the List command, echo the status (and perform the command elsewhere)
         /// </summary>
         /// <param name="data">TFMS_Data object containing the List command</param>
-        static void handleListRequest(TFMS_Data data)
+        private static void handleListRequest(TFMS_Data data)
         {
             Console.WriteLine("{0} has requested a list of clients", data.strName);
         }
@@ -87,7 +87,7 @@ namespace Console_Server
         /// When a message is sent to be broadcast to other uses, attempt to log it in the SQL database
         /// </summary>
         /// <param name="data">TFMS_Data object containing the Relay command</param>
-        static void handleRelayRequest(TFMS_Data data)
+        private static void handleRelayRequest(TFMS_Data data)
         {
             Console.WriteLine("{0} has sent a message", data.strName);
             
@@ -95,15 +95,11 @@ namespace Console_Server
             logMessage(data);
         }
 
-        #endregion
-
-        #region TFMS_Server_Main database logging
-
         /// <summary>
         /// Attempt to log the current message in the SQL database
         /// </summary>
         /// <param name="data">TFMS_Data object containing the current message</param>
-        static void logMessage(TFMS_Data data)
+        private static void logMessage(TFMS_Data data)
         {
             Console.WriteLine("Logging {0}'s message to database", data.strName);
 
